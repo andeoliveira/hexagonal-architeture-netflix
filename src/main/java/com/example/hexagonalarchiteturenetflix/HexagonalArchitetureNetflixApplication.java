07c7bd2a0a329d9cb219488db61afaef282894ba
andeoliveira <andherrson@hotmail.com>
2021-08-21 07:14:18 -0300

    First commit!

---
+++ src/main/java/com/example/hexagonalarchiteturenetflix/HexagonalArchitetureNetflixApplication.java
@@ -0,0 +1,13 @@
+package com.example.hexagonalarchiteturenetflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalArchitetureNetflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonalArchitetureNetflixApplication.class, args);
+	}
+
+}
